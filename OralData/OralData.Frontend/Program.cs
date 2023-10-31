ec5300ec2ca166566368b5a6ada22e545d1939fb
Juan David Nanclares <hvfbankubt@gmail.com>
2023-10-30 21:40:17 -0500

    user registration

--- OralData/OralData.Frontend/Program.cs
+++ OralData/OralData.Frontend/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using OralData.Frontend;
 using OralData.Frontend.Auth;
 using OralData.Frontend.Repositories;
+using OralData.Frontend.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -15,7 +16,10 @@ builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri("http
 builder.Services.AddScoped<IRepository, Repository>();
 builder.Services.AddSweetAlert2();
 builder.Services.AddAuthorizationCore();
-builder.Services.AddScoped<AuthenticationStateProvider, AuthenticationProviderTest>();
+builder.Services.AddScoped<AuthenticationProviderJWT>();
+builder.Services.AddScoped<AuthenticationStateProvider, AuthenticationProviderJWT>(x => x.GetRequiredService<AuthenticationProviderJWT>());
+builder.Services.AddScoped<ILoginService, AuthenticationProviderJWT>(x => x.GetRequiredService<AuthenticationProviderJWT>());
+
 
 
 await builder.Build().RunAsync();
