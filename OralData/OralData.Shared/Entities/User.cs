373de233e0afaa56d65456562894b8c7638f907a
KarenCalleB <karencalle07@gmail.com>
2023-11-26 14:19:53 -0500

    test commit

--- OralData/OralData.Shared/Entities/User.cs
+++ OralData/OralData.Shared/Entities/User.cs
@@ -8,7 +8,7 @@ namespace OralData.Shared.Entities
     {
         [Display(Name = "Documento")]
         [MaxLength(20, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
-        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio!!")]
         public string Document { get; set; } = null!;
 
         [Display(Name = "Nombres")]
