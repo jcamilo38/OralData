467f3c07f55ff7f6156ed39f8877406aec4c7e6d
Juan David Nanclares <hvfbankubt@gmail.com>
2023-11-26 10:46:55 -0500

    Unit_Test Specialtie Controller

---
+++ OralData/OralData.UnitTest/Controllers/GenericController.Test.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using OralData.Backend.Controllers;
+using OralData.Backend.Data;
+using OralData.Backend.Interfaces;
+using OralData.Shared.Entities;
+using Orders.Shared.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OralData.UnitTest.Controllers
+{
+    [TestClass]
+    public class GenericControllerTest
+
+    {
+        private readonly DbContextOptions<DataContext> _options;
+        private readonly Mock<IGenericUnitOfWork<Specialtie>> _UnitOfWorkmock = null!;
+
+        public GenericControllerTest()
+        {
+            _options = new DbContextOptionsBuilder<DataContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+            _UnitOfWorkmock = new Mock<IGenericUnitOfWork<Specialtie>>();
+        }
+
+        [TestMethod]
+
+        public async Task GetAsync_ReturnsOkResult()
+        {
+            // Arrange
+            using var context = new DataContext(_options);
+            var controller = new SpecialtieController(_UnitOfWorkmock.Object, context);
+            var pagination = new PaginationDTO { Filter = "Some" };
+
+            // Act
+            var result = await controller.GetAsync(pagination) as OkObjectResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(200, result.StatusCode);
+
+            // Clean Up
+            context.Database.EnsureDeleted();
+            context.Dispose();
+
+        }
+    }
+}
