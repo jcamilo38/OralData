e664cc60826bc56b9e6c798b7c88bac2fb855a7c
JUAN CAMILO OSSA GUZMAN <juanossa49114@correo.itm.edu.co>
2023-11-26 13:07:30 -0500

    Create MsTest  and firts UnitTest AccountcontrollerTest

--- OralData/OralData.Frontend/Program.cs
+++ OralData/OralData.Frontend/Program.cs
@@ -13,7 +13,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5203/") });
+builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7113/") });
 builder.Services.AddScoped<IRepository, Repository>();
 builder.Services.AddSweetAlert2();
 builder.Services.AddAuthorizationCore();
